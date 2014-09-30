8632bdc26726399d63000a33294342f7bf0871ab
Maksim Chemerisuk <chemerisuk@gmail.com>
2014-10-01 00:53:45 +0300

    minor refactoring for clarity

--- src/better-i18n.js
+++ src/better-i18n.js
@@ -18,16 +18,20 @@
         if (keyType === "string") {
             if (langIndex === -1) {
                 langIndex = languages.push(lang) - 1;
+
                 // add global rule for the data-i18n-{lang} attribute
                 DOM.importStyles(":lang(" + lang + ") > [data-i18n]", "display:none");
                 DOM.importStyles(":lang(" + lang + ") > [data-i18n=" + lang + "]", "display:inline");
             }
 
             if (!strings[key]) strings[key] = [];
+
             // store localized string internally
             strings[key][langIndex] = value;
         } else if (keyType === "object") {
-            Object.keys(key).forEach(function(x) { DOM.importStrings(lang, x, key[x]) });
+            Object.keys(key).forEach(function(x) {
+                DOM.importStrings(lang, x, key[x]);
+            });
         } else {
             throw TypeError("importStrings");
         }
@@ -38,34 +42,39 @@
     };
 
     function Entry(key) {
-        var record = strings[key] || {};
-
-        // populate language-specific values
-        languages.forEach(function(lang, index) {
-            if (index in record) {
-                this[lang] = record[index];
-            }
-        }, this);
+        languages.forEach(populateLang(key, this));
 
         this._ = key;
     }
 
     Entry.prototype.toString = function(varMap) {
-        var result = Object.keys(this).map(function(key) {
-            var lang = key === "_" ? "" : key,
-                value = DOM.format(this[key], varMap);
-
-            return "<span data-i18n=\"" + lang + "\">" + value + "</span>";
-        }, this);
-
-        return result.join("");
+        return Object.keys(this).map(formatLang(varMap, this)).join("");
     };
 
     Entry.prototype.toLocaleString = function(lang) {
-        if (!lang) lang = document.documentElement.lang;
+        if (!lang) lang = DOM.get("lang");
 
         return lang in this ? this[lang] : this._;
     };
+
+    function populateLang(key, obj) {
+        var record = strings[key] || {};
+
+        return function(lang, index) {
+            if (index in record) {
+                obj[lang] = record[index];
+            }
+        };
+    }
+
+    function formatLang(varMap, obj) {
+        return function(key) {
+            var lang = key === "_" ? "" : key,
+                value = DOM.format(obj[key], varMap);
+
+            return "<span data-i18n=\"" + lang + "\">" + value + "</span>";
+        };
+    }
 
     // by default just show data-i18n attribute value
     DOM.importStyles("[data-i18n]", "display:none");
