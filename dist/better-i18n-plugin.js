fc26ac6be6a62176334cb5cecac5bb69fab94700
Maksim Chemerisuk <chemerisuk@gmail.com>
2014-10-17 18:48:12 +0300

    version 1.0.0-rc.2

--- dist/better-i18n-plugin.js
+++ dist/better-i18n-plugin.js
@@ -1,10 +1,9 @@
 /**
- * @file src/better-i18n.js
- * @version 1.0.0-rc.1 2014-10-03T02:23:09
- * @overview Internationalization extension for better-dom
- * @copyright Maksim Chemerisuk 2014
+ * better-i18n-plugin: Internationalization plugin for better-dom
+ * @version 1.0.0-rc.2 Fri, 17 Oct 2014 15:48:12 GMT
+ * @link 
+ * @copyright 2014 Maksim Chemerisuk
  * @license MIT
- * @see https://github.com/chemerisuk/better-i18n
  */
 (function(DOM) {
     "use strict";
@@ -21,14 +20,13 @@
                 langIndex = languages.push(lang) - 1;
 
                 // add global rules to to able to switch to new language
-                var prefix = ":lang(" + lang + ") > ";
-
+                var prefix = ((":lang(" + lang) + ") > ");
                 // by default localized strings should be hidden
-                DOM.importStyles("[data-i18n='" + lang + "']", "display:none");
+                DOM.importStyles((("[data-i18n=\"" + lang) + "\"]"), "display:none");
                 // ... except current page language is appropriate
-                DOM.importStyles(prefix + "[data-i18n='" + lang + "']", "display:inline");
+                DOM.importStyles((("" + prefix) + ("[data-i18n=\"" + lang) + "\"]"), "display:inline");
                 // ... in such case hide default string as well
-                DOM.importStyles(prefix + "[data-i18n='" + lang + "'] ~ [data-i18n]", "display:none");
+                DOM.importStyles((("" + prefix) + ("[data-i18n=\"" + lang) + "\"] ~ [data-i18n]"), "display:none");
             }
 
             if (!strings[key]) strings[key] = [];
@@ -36,7 +34,7 @@
             // store localized string internally
             strings[key][langIndex] = value;
         } else if (keyType === "object") {
-            Object.keys(key).forEach(function(x) {
+            Object.keys(key).forEach(function(x)  {
                 DOM.importStrings(lang, x, key[x]);
             });
         } else {
@@ -44,11 +42,7 @@
         }
     };
 
-    DOM.i18n = function(key, varMap) {
-        return new Entry(key, varMap);
-    };
-
-    // helper functions
+    DOM.i18n = function(key, varMap)  {return new Entry(key, varMap)};
 
     function Entry(key, varMap) {
         languages.forEach(populateLang(key, varMap, this));
@@ -56,20 +50,24 @@
         this._ = DOM.format(key, varMap);
     }
 
-    Entry.prototype.toString = function(varMap) {
-        return Object.keys(this).map(formatLang(varMap, this)).join("");
+    Entry.prototype = {
+        toString: function(varMap) {
+            return Object.keys(this).map(formatLang(varMap, this)).join("");
+        },
+
+        toLocaleString: function(lang) {
+            if (!lang) lang = DOM.get("lang");
+
+            return lang in this ? this[lang] : this._;
+        }
     };
 
-    Entry.prototype.toLocaleString = function(lang) {
-        if (!lang) lang = DOM.get("lang");
-
-        return lang in this ? this[lang] : this._;
-    };
+    // helper functions
 
     function populateLang(key, varMap, entry) {
         var record = strings[key] || {};
 
-        return function(lang, index) {
+        return function(lang, index)  {
             if (index in record) {
                 entry[lang] = DOM.format(record[index], varMap);
             }
@@ -77,11 +75,11 @@
     }
 
     function formatLang(varMap, entry) {
-        return function(key) {
+        return function(key)  {
             var lang = key === "_" ? "" : key,
                 value = DOM.format(entry[key], varMap);
 
-            return "<span data-i18n=\"" + lang + "\">" + value + "</span>";
+            return (("<span data-i18n=\"" + lang) + ("\">" + value) + "</span>");
         };
     }
 }(window.DOM));
