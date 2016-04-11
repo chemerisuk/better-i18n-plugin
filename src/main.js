3abe2cc5b9c0a1957a8bf28b9718eba6e7a29b4b
Maksim Chemerisuk <chemerisuk@gmail.com>
2016-04-11 13:10:01 +0300

    minor refactoring

--- src/main.js
+++ src/main.js
@@ -1,43 +1,45 @@
-/* jshint -W053 */
 (function(DOM) {
     "use strict";
 
     var strings = [],
-        languages = [];
+        languages = [],
+        HTML = DOM.get("documentElement");
 
     function formatKey(key, args, start = 0) {
         return key.replace(/%s/g, (str) => args[start++] || str);
     }
 
-    function Entry(key, args) {
-        languages.forEach((lang, index) => {
-            var value = strings[index][key];
+    class Entry {
+        constructor(key, args) {
+            languages.forEach((lang, index) => {
+                var value = strings[index][key];
 
-            if (value) {
-                if (args) value = formatKey(value, args);
+                if (value) {
+                    if (args) value = formatKey(value, args);
 
-                this[lang] = value;
-            }
-        });
+                    this[lang] = value;
+                }
+            });
 
-        this._ = args ? formatKey(key, args) : key;
+            this._ = args ? formatKey(key, args) : key;
+        }
+
+        toString() {
+            // "_" key should always be the last one
+            return Object.keys(this)
+                .sort((key) => key === "_" ? 1 : -1)
+                .map((key) => `<span lang="${key}">${this[key]}</span>`)
+                .join("");
+        }
+
+        toLocaleString(lang) {
+            return this[lang || HTML.lang] || this._;
+        }
+
+        valueOf() {
+            return `<span>${this.toString()}</span>`;
+        }
     }
-
-    Entry.prototype.toString = function() {
-        // "_" key should always be the last one
-        var keys = Object.keys(this).sort((k) => k === "_" ? 1 : -1);
-
-        return keys.map((key) =>
-            `<span lang="${key}">${this[key]}</span>`).join("");
-    };
-
-    Entry.prototype.valueOf = function() {
-        return `<span>${this.toString()}</span>`;
-    };
-
-    Entry.prototype.toLocaleString = function(lang) {
-        return this[lang || DOM.get("documentElement").lang] || this._;
-    };
 
     DOM.importStrings = function(lang, key, value) {
         if (typeof lang !== "string") {
